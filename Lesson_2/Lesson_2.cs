82a08710541f028a2df5f6ddf5f5692fd04eb389
Junkwolves <justajunk2069@gmail.com>
2019-01-24 16:29:40 +0200

    Lesson_2
    Solution 2

---
+++ Lesson_2/Lesson_2.cs
@@ -0,0 +1,19 @@
+namespace Lesson_2
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            
+            System.Console.WriteLine("Hello, world!");
+			
+			/*
+				System.Console.WriteLine("Hello, world!");
+			*/
+			
+			//  System.Console.WriteLine("Hello, world!");
+			
+			System.Console.WriteLine("Hello, world!");
+        }
+    }
+}
