629ba04ca301d2032e99c09a8b5d1bca25a72d69
Junkwolves <justajunk2069@gmail.com>
2019-01-24 16:31:46 +0200

    Lesson_4
    Solution 4

---
+++ Lesson_4/Lesson_4.cs
@@ -0,0 +1,38 @@
+using System;
+
+namespace Lesson_4
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+
+        	// Data_Type Name = Value
+
+        	int a = 3;
+
+        	int b;
+
+        	b = 5;
+
+        	int c = a + b;
+
+        	Console.WriteLine("\n" + "   a = " + a);
+
+        	Console.WriteLine("   b = " + b);
+
+        	Console.WriteLine("   c = a + b = " + c + "\n");
+
+        	char f = 'H';
+        	char g = 'i';
+
+        	Console.WriteLine("   " + f + g + "\n");
+
+        	string o = "   Hello";
+        	string k = ", world!";
+
+        	Console.WriteLine( o + k + "\n");
+
+        }
+    }
+}
