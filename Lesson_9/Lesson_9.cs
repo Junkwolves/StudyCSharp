72630c83cfb706e6e4899c9ff3ebaa6498253912
Junkwolves <justajunk2069@gmail.com>
2019-02-18 18:34:33 +0200

    Lesson_9
    Solution 9

---
+++ Lesson_9/Lesson_9.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Lesson_9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            double Res_1 = 1 + 2 - 2 * 5 / 45;
+
+            double Res_2 = 1 - 2 + 2 / 5 * 45;
+
+            double Res_3 = 1 / 2 * 2 + 5 - 45;
+
+            double Res_4 = 1 / 2 * 2 - 5 + 45;
+
+            Console.WriteLine("\n   1 = " + Res_1);
+
+            Console.WriteLine("\n   2 = " + Res_2);
+
+            Console.WriteLine("\n   3 = " + Res_3);
+
+            Console.WriteLine("\n   4 = " + Res_4 + "\n");
+
+        }
+    }
+}
