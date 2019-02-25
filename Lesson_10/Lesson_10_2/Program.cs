7eeb8c2edd170206a489e3029963525b334676ed
Junkwolves <justajunk2069@gmail.com>
2019-02-25 18:15:16 +0200

    Lesson_10
    Solution 10

---
+++ Lesson_10/Lesson_10_2/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Lesson_10_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World! 2");
+            Console.ReadLine();
+        }
+    }
+}
